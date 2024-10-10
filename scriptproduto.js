35df5acfcc0e0e684b03c9132bc7827b91311dd6
PedroWilsonRL <designergraficopedrowilson@gmail.com>
2024-10-10 18:38:14 -0300

    Update scriptproduto.js

--- scriptproduto.js
+++ scriptproduto.js
@@ -4,15 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener('click', () => {
-            // Armazena o src e alt da imagem principal
             const mainSrc = mainImage.src;
             const mainAlt = mainImage.alt;
 
-            // Atualiza a imagem principal com a da miniatura clicada
             mainImage.src = thumbnail.src;
             mainImage.alt = thumbnail.alt;
 
-            // Atualiza a miniatura com a imagem antiga da principal
             thumbnail.src = mainSrc;
             thumbnail.alt = mainAlt;
         });
